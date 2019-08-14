2cf8175bd66d0501417b317e01e927e21e50c58e
edgar fernandez <eddyfer_77@hotmail.com>
2019-08-14 13:28:07 +0200

    registro inicio sesion activar cuenta por email pagina principal y div principal terminado etapa 1

--- www/js/mostrarIU.js
+++ www/js/mostrarIU.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    inicioSesion();
 	 /* mostrar tabs divInicio inicio sesion y registrar*/            
     $("#aRegistrar").click(function(){
         $("#aRegistrar").tab('show');
@@ -10,32 +11,75 @@ $(document).ready(function(){
     $("#linkRegistrar").click(function(){
         $("#aRegistrar").tab('show');
     });
+    $("#icoBuscar").click(function(){
+        //event.preventDefault();
+        $(".icoFooter .glyphicon-search").css({"color":"#008080"});
+        $(".icoFooter .glyphicon-envelope").css({"color":"rgb(128, 128, 128,.8)"});
+        
+        $(".icoFooter .glyphicon-user").css({"color":"rgb(128, 128, 128,.8)"});
+        $(".icoFooter .glyphicon-upload").css({"color":"rgb(128, 128, 128,.8)"});
+
+        /*$("#icoBuscar").css({"transform":"scale(1.5,1)","width":"25%","color":"#00cccc"});        
+        $("#icoSolicitud").css({"transform":"scale(1,1)","width":"15%"});
+        $("#icoChat").css({"transform":"scale(1,1)","width":"15%"});
+        $("#icoUsuario").css({"transform":"scale(1,1)","width":"15%"});
+        $("#icoPublicar").css({"transform":"scale(1,1)","width":"15%"});
+        */
+        $("#paginaPrincipal").css("display", "block");
+        $("#paginaListaMapas").css("display", "none");            
+        $("#paginaSolicitud").css("display", "none");
+        $("#paginaChat").css("display", "none");            
+        $("#paginaUsuarioPerfil").css("display", "none");
+        $("#paginaPublicar").css("display", "none");                        
+
+        $("#icoFMBuscar").css({"color":"#008080"});
+        $("#icoFMSolicitud").css({"color":"rgb(128, 128, 128,.8)"});            
+        $("#icoFMChat").css({"color":"rgb(128, 128, 128,.8)"});            
+        $("#icoFMPerfil").css({"color":"rgb(128, 128, 128,.8)"});
+        $("#icoFMPublicar").css({"color":"rgb(128, 128, 128,.8)"});                       
+    });
 })
-	function registrarUsuario(){ //evento activado por onsubmit en validarformulario.js
-		event.preventDefault();
-		$('mID').html("");
-        $('mIS').html("");
-    	$.ajax({
-        	type : 'POST',
-            url: 'http://127.0.0.1/wasiWeb/php/registrar.php',
-        	data:new FormData($('#formRegistro')[0]),
-        	dataType: 'json',
-        	cache: false,
-        	contentType: false,
-        	processData: false,
-        	success: function(datosR)
-        	{  $("#aIngresar").tab('show');
-            	if(datosR.uReg==1){
-                	$('#mIS').html(datosR.msg + " " + datosR.umEmail);}
-                	if(datosR.uReg==0){
-                    	$('#mID').html(datosR.msg);}
+function inicioSesion(){
+    $datosLocal=JSON.parse(localStorage.getItem('datosInicioSesion'));
+    if($datosLocal!= undefined || $datosLocal!= null ){
+       /* $.each($datosLocal, function(key, value){alert(key + ' = ' + value);});*/
+        $("#nombreCompleto").html($datosLocal['usrName']);//http://192.168.1.145/wasiWeb/
+        $("#imgPerfil").attr({"src":"http://127.0.0.1/wasiWeb/"+ $datosLocal['usrImg']});// fotos/eddyfer_77@hotmail.com/paisaje1.jpg}); //mostramos foto por defecto 
+          
+        //$("#imgP").html($datosLocal['usrImg']);
+        $("#divInicio").css("display", "none");
+        $("#divPrincipal").css("display", "block");
+        $("body").css("background","#f2f2f2");
+        $idCliente = $datosLocal['usrId'];
+        $emailCliente = $datosLocal['usrEmail'];    
+        //alert($datosLocal['usrEmail'] + " " + $datosLocal['usrId']);
+    }
+}
+function registrarUsuario(){ //evento activado por onsubmit en validarformulario.js
+	event.preventDefault();
+	$('mID').html("");
+    $('mIS').html("");
+    $.ajax({
+       	type : 'POST',
+        url: 'http://127.0.0.1/wasiWeb/php/registrar.php',
+       	data:new FormData($('#formRegistro')[0]),
+       	dataType: 'json',
+       	cache: false,
+       	contentType: false,
+       	processData: false,
+       	success: function(datosR)
+       	{  $("#aIngresar").tab('show');
+           	if(datosR.uReg==1){
+               	$('#mIS').html(datosR.msg + " " + datosR.umEmail);}
+               	if(datosR.uReg==0){
+                   	$('#mID').html(datosR.msg);}
                 //alert(datosR.uReg);
-        	},
-        	error : function(jqXHR, textStatus, errorThrown) {
-                alert(jqXHR.status +" "+ textStatus+" "+ errorThrown);
-        	}
-    	});    
-	}
+       	},
+       	error : function(jqXHR, textStatus, errorThrown) {
+            alert(jqXHR.status +" "+ textStatus+" "+ errorThrown);
+       	}
+    });    
+}
 function iniciarSession(){
     $('#mIS').html(" ");
     $('#mID').html(" ");
